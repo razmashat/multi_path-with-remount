63bc6c8db841e48c4e1a262032e6ae87f2830404
razmashat <razmashat@gmail.com>
2018-07-04 16:52:52 +0300

    a
    a

---
+++ multi_path/keepFile.h
@@ -0,0 +1,14 @@
+//
+//  keepFile.h
+//  multi_path
+//
+//  Created by Lakr Sakura on 2018/7/4.
+//  Copyright © 2018 Ian Beer. All rights reserved.
+//
+
+#ifndef keepFile_h
+#define keepFile_h
+
+#include <stdio.h>
+int initkeepFile(void);
+#endif /* keepFile_h */
